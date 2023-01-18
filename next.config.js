9dc846ef31db59edbbb4184a0dbc064a07c123bd
unknown <guka20>
2023-01-19 02:33:28 +0400

    main

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { getBaseUrl } from "nextjs-url";
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    BASE_URL: "http://localhost:3000/",
+    BASE_URL: getBaseUrl().href,
     MONGO_URI:
       "mongodb+srv://gurami:!Guka1234@cluster0.od4ofq2.mongodb.net/?retryWrites=true&w=majority",
   },
