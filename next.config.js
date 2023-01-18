85b1bfd5f2d89de9a8e9314c1baf0c960490642f
unknown <guka20>
2023-01-19 00:18:02 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BASE_URL: "https://next-and-mongodb.vercel.app/",
+    BASE_URL: "https://next-and-mongodb.vercel.app",
     MONGO_URI:
       "mongodb+srv://gurami:!Guka1234@cluster0.od4ofq2.mongodb.net/?retryWrites=true&w=majority",
   },
