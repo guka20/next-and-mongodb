a971cb7fd8ffda6be1103466cdd878e999cf284b
unknown <guka20>
2023-01-19 02:15:45 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   env: {
     BASE_URL: "http://localhost:3000/",
     MONGO_URI:
